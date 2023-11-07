e00ee0c7b5ac2ca92eae05d8df81d6c645fce374
Camila Rios <mriosm@unal.edu.co>
2023-11-07 06:39:58 -0500

    c

---
+++ src/gestorAplicacion/ServiciosClientes.py
@@ -0,0 +1,29 @@
+from gestorAplicacion import Cliente
+
+class ServiciosClientes:
+    codigoReferencia = 0
+    count =  0
+
+    def __init__(self, clientes == "Anonimo", text):
+    self._cliente = clientes
+    self._razon = text
+    codigoReferencia += count
+    count += 1
+
+    def setCliente(self, cliente):
+        self._cliente = cliente
+    
+    def getCliente(self):
+        return self._cliente
+    
+    def setRazon(self, text):
+        self._razon = text
+    
+    def getRazon(self):
+        return self._razon
+    
+    def getCodigoRerencia(self):
+        return self.codigoReferencia
+    
+
+    
