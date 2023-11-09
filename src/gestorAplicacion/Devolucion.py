e523342814d586b4b3cee380165a102f2f700d52
Camila Rios <mriosm@unal.edu.co>
2023-11-09 07:38:41 -0500

    s

--- src/gestorAplicacion/Devolucion.py
+++ src/gestorAplicacion/Devolucion.py
@@ -11,6 +11,7 @@ class Devolucion (ServiciosClientes):
         self._cedula.setCedula(cedula) #Pendiente revisar ID
         self._correo.setEmail(correo)#Pendiente revisar Correo
         self._estado = "Pendiente"
+        self._motivo = None
 
         factura = Factura.buscarFactura(numerofactura)
         self._factura = factura
@@ -22,6 +23,34 @@ class Devolucion (ServiciosClientes):
     def getEstado(self):
         return self._estado
     
+    def setMotivo(self, texto):
+        self._motivo = texto
+    
+    def getMotivo(self):
+         return self._motivo
+    
+    def __str__(self):
+        a = None
+
+        if self.getEstado() == "Denegado": 
+            a = "N° Solicitud: " + self.getCodigoReferencia() + "\n" + "Cliente: " + self._cliente.getNombre() + " - CC. " + self._cliente.getCedula() + " - Email: " + self._cliente.getEmail() + "\n" + "Estado de la solicitud:  " + self.getEstado()+ "\n" + "Motivo de rechazo: " + self.getMotivo() + "\n" + "La solicitud fue denegada, se enviara un correo al cliente con la decision tomada"
+        
+        elif self.getEstado() == "Aprobado":
+            a = "N° Solicitud: " + self.getCodigoReferencia() + "\n" + "Cliente: " + self._cliente.getNombre() + " - CC. " + self._cliente.getId() + " - Email: " + self._cliente.getEmail() + "\n" + "Estado de la solicitud:  " + self.getEstado()+ "\n" + "La solicitud fue aprobada, se enviara un correo al cliente con la decision tomada y los pasos a seguir para hacer efectiva su solicitud"
+        
+        else: 
+            a = "N° Solicitud: " + self.getCodigoReferencia() + "\n" + "Cliente: " + self._cliente.getNombre() + " - CC. " + self._cliente.getId() + " - Email: " + self._cliente.getEmail() + "\n" + "Codigo de Factura: " + self._factura.getCodigo() + "\n" + "Estado de la solicitud:  " + self.getEstado()+ "\n" + "Motivo de la solicitud: " + self.getTexto()
+        
+    @classmethod
+    def DenegarDevolucion(cls, codigo):
+        a = cls.buscarDevolucion(codigo)
+        a.setEstado("Denegado")
+
+    @classmethod
+    def AprobarDevolucion(cls, codigo):
+        a = cls.buscarDevolucion(codigo)
+        a.setEstado("Aprobado")
+        
     @classmethod
     def getDevoluciones(cls):
         return cls.Devoluciones
