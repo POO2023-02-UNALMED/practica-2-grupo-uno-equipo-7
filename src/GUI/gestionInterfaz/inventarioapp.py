3bc6550074b60bad67437499cee5efd730089a70
otaku9820 <aguido@unal.edu.co>
2023-11-11 11:40:15 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaa22222222

--- src/GUI/gestionInterfaz/inventarioapp.py
+++ src/GUI/gestionInterfaz/inventarioapp.py
@@ -1,12 +1,20 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import Frame
+from tkinter import ttk
 
 class inventarioapp(Frame):
     def __init__(self, padre,  controlador):
         super().__init__(padre)
         self.controlador=controlador
-        self.configure(background="red")
+        self.configure(background="white")
+        label1=tk.Label(self, text="Inventario", font=("Arial",35),fg="pink")
+        label1.pack(side="top", pady=10)
+        valor_defecto=tk.StringVar(value="Sedes")
+        combo=ttk.Combobox(self, values=["envigado","sandiego","La Amercia", "belen"], textvariable=valor_defecto)
+        combo.pack(side="bottom")
+        
+        
         
         
         
