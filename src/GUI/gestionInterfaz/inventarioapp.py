c4c35d3f31948f02d8e25f26a4adabefd152c026
otaku9820 <aguido@unal.edu.co>
2023-11-11 18:57:41 -0500

    me funciona

--- src/GUI/gestionInterfaz/inventarioapp.py
+++ src/GUI/gestionInterfaz/inventarioapp.py
@@ -8,12 +8,16 @@ class inventarioapp(Frame):
         super().__init__(padre)
         self.controlador=controlador
         self.configure(background="white")
-        label1=tk.Label(self, text="Inventario", font=("Arial",35),fg="pink")
-        label1.grid(row=0, column=0, columnspan=2, pady=10)
+        label1=tk.Label(self, text="Inventario", font=("Arial",40),fg="pink")
+        label1.grid(row=0, column=2, columnspan=2, pady=10)
         valor_defecto=tk.StringVar(value="Sedes")
-        combo=ttk.Combobox(self, values=["envigado","sandiego","La Amercia", "belen"], textvariable=valor_defecto,width="30")
+        combo_style = ttk.Style()
+        
+        # Crea un nuevo estilo personalizado (My.TCombobox) y ajusta la altura (padding)
+        combo_style.configure('My.TCombobox', padding=[20, 5, 90, 5])
+        combo=ttk.Combobox(self, values=["envigado","sandiego","La Amercia", "belen"], textvariable=valor_defecto,style='My.TCombobox')
         combo.grid(row=1, column=1, padx=10, pady=10, sticky="w")
-        otro_label = tk.Label(self, text="seleccionar sede ")
+        otro_label = tk.Label(self, text="seleccionar sede ", font=("Arial", 20),bg="white")
         otro_label.grid(row=1, column=0, padx=10, pady=10, sticky="e")
         
         
