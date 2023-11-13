a03836a14939e1942ffbcc504992b683a6bd222a
dsilvau <125933418+dsilvau@users.noreply.github.com>
2023-11-13 11:02:00 -0500

    GUI5

--- src/main2.py
+++ src/main2.py
@@ -15,6 +15,17 @@ if __name__ == "__main__":
     plato2 = Plato("Tostadas", ["Tostada", "Carne", "Cebolla", "Tomate", "Salsa"],20)
     plato3 = Plato("Sopes", ["Sope", "Carne", "Cebolla", "Tomate", "Salsa"],30)
     plato4 = Plato("Enchiladas", ["Tortilla", "Carne", "Cebolla", "Tomate", "Salsa"],30)
+    plato5 = Plato("Quesadillas", ["Tortilla", "Queso"],10)
+
+    Factura(1, 1, 1, "2021-01-01", 10, [plato1, plato2])
+    Factura(2, 2, 2, "2021-02-01", 20, [plato2, plato3])
+    Factura(3, 3, 3, "2021-03-01", 30, [plato3, plato4])
+    Factura(1, 1, 1, "2021-04-01", 40, [plato4, plato5])
+    Factura(2, 2, 2, "2021-05-01", 50, [plato5, plato1])
+    Factura(3, 3, 3, "2021-06-01", 60, [plato1, plato2])
+    Factura(1, 1, 1, "2021-07-01", 70, [plato2, plato3])
+    Factura(2, 2, 2, "2021-08-01", 80, [plato3, plato4])
+    Factura(3, 3, 3, "2021-09-01", 90, [plato4, plato5])
 
     
     
