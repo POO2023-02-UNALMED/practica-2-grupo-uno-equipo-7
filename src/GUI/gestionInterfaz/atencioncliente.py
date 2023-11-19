d86f749af887a70b97390822a5f454e0322bbb7b
Camila Rios <mriosm@unal.edu.co>
2023-11-18 23:45:35 -0500

    c

--- src/GUI/gestionInterfaz/atencioncliente.py
+++ src/GUI/gestionInterfaz/atencioncliente.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 class AtencionCliente(Frame):
+
     def __init__(self, padre, controlador):
         super().__init__(padre)
         self.controlador = controlador
@@ -9,19 +10,22 @@ class AtencionCliente(Frame):
         label1 = Label(self, text="Bienvenido al centro de Atencion al cliente\n¿Que desea realizar?", bg = "#a19f9f", font=("Roboto", 20), fg="#0a0a0a")
         label1.grid(row=0, column=2, columnspan=2, pady=10)
 
-        botonSugerencia = Button(self, text="Sugerencia", command=self._mostrarNombreYPlatoPreferido, font=("Roboto", 12), fg="#0a0a0a")
+        botonSugerencia = Button(self, text="Sugerencia", command=self.mostrar, font=("Roboto", 12), fg="#0a0a0a")
         botonSugerencia.pack(side=TOP, fill=BOTH, padx=10, pady=10)
 
-        botonQueja = Button(self, text="Queja", command=self._mostrarNombreYPlatoPreferido, font=("Roboto", 12), fg="#0a0a0a")
+        botonQueja = Button(self, text="Queja", command=self.mostrar, font=("Roboto", 12), fg="#0a0a0a")
         botonQueja.pack(side=TOP, fill=BOTH, padx=10, pady=10)
 
-        botonQueja = Button(self, text="Queja", command=self._mostrarNombreYPlatoPreferido, font=("Roboto", 12), fg="#0a0a0a")
+        botonQueja = Button(self, text="Queja", command=self.mostrar, font=("Roboto", 12), fg="#0a0a0a")
         botonQueja.pack(side=TOP, fill=BOTH, padx=10, pady=10)
 
-        botonReseña = Button(self, text="Reseña", command=self._mostrarNombreYPlatoPreferido, font=("Roboto", 12), fg="#0a0a0a")
+        botonReseña = Button(self, text="Reseña", command=self.mostrar, font=("Roboto", 12), fg="#0a0a0a")
         botonReseña.pack(side=TOP, fill=BOTH, padx=10, pady=10)
 
-        botonDevolucion = Button(self, text="Devolución", command=self._mostrarNombreYPlatoPreferido, font=("Roboto", 12), fg="#0a0a0a")
+        botonDevolucion = Button(self, text="Devolución", command=self.mostrar, font=("Roboto", 12), fg="#0a0a0a")
         botonDevolucion.pack(side=TOP, fill=BOTH, padx=10, pady=10)
+
+    def mostrar(self):
+        print("Prueba")
 
     
