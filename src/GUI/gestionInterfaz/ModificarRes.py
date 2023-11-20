12748b8142335026a12765ea4fd78a8cc6b091c2
LuaN1409 <141962309+LuaN1409@users.noreply.github.com>
2023-11-20 02:22:38 -0500

    ....

--- src/GUI/gestionInterfaz/ModificarRes.py
+++ src/GUI/gestionInterfaz/ModificarRes.py
@@ -42,24 +42,11 @@ class ModificarRes(tk.Frame):
         self._entradaId = Entry(frameEntrada, font=FONT2)
         self._entradaId.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
         
-        
-        # Nombre
-        frameEntrada1 = Frame(self, bg=BACKGROUND_CONTENEDOR)
-        frameEntrada1.pack(side=TOP, fill=BOTH, padx=10, pady=10)
-
-        # Se inicializa la etiqueta para el id del cliente
-        labelnm = Label(frameEntrada1, text="Nombre del cliente", bg=BACKGROUND_CONTENEDOR, font=FONT2, fg=FG)
-        labelnm.grid(row=1, column=0, padx=10, pady=10, sticky="w")
-
-        # Se inicializa la entrada para el id del cliente
-        self._entradanm = Entry(frameEntrada1, font=FONT2)
-        self._entradanm.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
-        
-        boton_reserva = tk.Button(self, text="Buscar", height=1, command=lambda: self.buscarReserva(self._entradaId.get(), self._entradanm.get()))
+        boton_reserva = tk.Button(self, text="Buscar", height=1, command=lambda: self.buscarReserva(self._entradaId.get()))
         boton_reserva.pack(pady=20)
         
         
-    def buscarReserva(self, id, nombre): 
+    def buscarReserva(self, id): 
         clienteId =  self._entradaId.get()
         
         try:
